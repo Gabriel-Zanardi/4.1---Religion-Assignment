8e13f267682f88ca25ec44670874245c2361a55f
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2025-04-14 02:26:40 +0000

    fixed typo

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function myButtonClicked() {
     <p>I found that the Sh'ma is a way of us understanding God in us. A way we can always remind ourselves as his creation, as his children.</p>
     <p>We are his son and daughter, and reciting that to ourselves and children is a way to invite God not only into our hearts, but into our minds and residences.</p>
     <p>This is similar to the CSTs and how we are always reminded to love ourselves, others.</p>
-    <p>The Sh'ma cana also connect to the lesson 3.1 in Indigenous Spirituality where it talked about words and their powers, as if they're both summonning good upbringing to our hearts and minds </p>
+    <p>The Sh'ma can also connect to the lesson 3.1 in Indigenous Spirituality where it talked about words and their powers, as if they're both summonning good upbringing to our hearts and minds </p>
     <img src="./images/sh'ma-prayer-html.png" alt="Sh'ma prayer being performed" style="max-width: 100%; height: auto; margin-top: 10px;">
   `;
 }
